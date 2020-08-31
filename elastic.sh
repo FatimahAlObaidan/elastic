485042bc16747f4512eea6ba4d0bca6dd039fead
Ubuntu <ubuntu@ip-10-0-1-74.me-south-1.compute.internal>
2020-08-31 18:21:25 +0000

    fix

--- elastic.sh
+++ elastic.sh
@@ -5,7 +5,7 @@ helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
 helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=ommegaa
-helm install fluent-bit fluent/fluent-bit --namespace=elf
+helm install fluent-bit fluent/fluent-bit --namespace=ommegaa
 helm install kibana elastic/kibana --version=7.9.0 --namespace=ommegaa --set service.type=LoadBalancer
 kubectl run random-logger --image=chentex/random-logger -n ommegaa
 kubectl apply -f ingress.yaml -n ommegaa
