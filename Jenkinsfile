16a4dc3f85d9b24df8facaf8de3fae333a2bfb1c
Ubuntu <ubuntu@ip-10-0-1-74.me-south-1.compute.internal>
2020-08-31 18:14:24 +0000

    get all pods

--- Jenkinsfile
+++ Jenkinsfile
@@ -29,7 +29,7 @@ spec:
               container('kubectl') {
                   sh '''
 			source elastic.sh
-			kubectl --token=$TOKEN get all -n ommega
+			kubectl --token=$TOKEN get all -n ommegaa
 		      '''
               }
           }
--- elastic.sh
+++ elastic.sh
@@ -4,9 +4,9 @@ kubectl apply -f elastic.namespace.yaml
 helm repo add elastic https://helm.elastic.co
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
-helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=ommega
+helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=ommegaa
 helm install fluent-bit fluent/fluent-bit --namespace=elf
-helm install kibana elastic/kibana --version=7.9.0 --namespace=ommega --set service.type=LoadBalancer
-kubectl run random-logger --image=chentex/random-logger -n ommega
-kubectl apply -f ingress.yaml -n ommega
-kubectl create -n omegaa clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:default
+helm install kibana elastic/kibana --version=7.9.0 --namespace=ommegaa --set service.type=LoadBalancer
+kubectl run random-logger --image=chentex/random-logger -n ommegaa
+kubectl apply -f ingress.yaml -n ommegaa
+kubectl create -n ommegaa clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:default
