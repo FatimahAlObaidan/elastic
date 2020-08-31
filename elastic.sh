5c25fac9c9265317cda4cb3035c567c55a0294c6
Ubuntu <ubuntu@ip-10-0-1-120.me-south-1.compute.internal>
2020-08-31 19:03:10 +0000

    create new namespace

--- elastic.sh
+++ elastic.sh
@@ -6,7 +6,6 @@ helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update
 helm install elasticsearch elastic/elasticsearch --version=7.9.0 --namespace=omega
 helm install fluent-bit fluent/fluent-bit --namespace=omega
-helm install kibana elastic/kibana --version=7.9.0 --namespace=ommegaa --set service.type=LoadBalancer
+helm install kibana elastic/kibana --version=7.9.0 --namespace=omega --set service.type=LoadBalancer
 kubectl run random-logger --image=chentex/random-logger -n omega
 kubectl apply -f ingress.yaml -n omega
-kubectl create -n omega clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:default
